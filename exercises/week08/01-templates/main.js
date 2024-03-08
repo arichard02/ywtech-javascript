e2ecd01f2a236094e7313f4af103a31d44a29592
Adwaina <arichard02@aol.com>
2024-03-07 21:12:50 -0500

    Moved zip files, added CSS, modified template, added name var

--- exercises/week08/01-templates/main.js
+++ exercises/week08/01-templates/main.js
@@ -1,20 +1,21 @@
 const gallery = document.querySelector("#gallery");
 
-function addImageCardToDOM(url, caption) {
-    const template = `
+function addImageCardToDOM(url, caption, name) {
+  const template = `
         <section class="card">
             <img src="${url}" />
             <p>${caption}</p>
+            <p>${name}</p>
         </section>`;
-    gallery.innerHTML += template;
+  gallery.innerHTML += template;
 }
 
 // draw a card for each of the following images:
-addImageCardToDOM("https://picsum.photos/id/106/400/300", "Flowers");
-// addImageCardToDOM("https://picsum.photos/id/107/400/300", "Wild Grasses");
-// addImageCardToDOM("https://picsum.photos/id/108/400/300", "Palm Trees");
-// addImageCardToDOM("https://picsum.photos/id/109/400/300", "Meadow at sunset");
-// addImageCardToDOM("https://picsum.photos/id/110/400/300", "Field");
-// addImageCardToDOM("https://picsum.photos/id/111/400/300", "Truck");
-// addImageCardToDOM("https://picsum.photos/id/112/400/300", "Wheat");
-// addImageCardToDOM("https://picsum.photos/id/114/400/300", "Sky");
+addImageCardToDOM("https://picsum.photos/id/106/400/300", "Flowers", "Andrea");
+addImageCardToDOM("https://picsum.photos/id/107/400/300", "Wild Grasses", "Janice");
+addImageCardToDOM("https://picsum.photos/id/108/400/300", "Palm Trees", "Wanda");
+addImageCardToDOM("https://picsum.photos/id/109/400/300", "Meadow at sunset", "Anita");
+addImageCardToDOM("https://picsum.photos/id/110/400/300", "Field", "Christina");
+addImageCardToDOM("https://picsum.photos/id/111/400/300", "Truck", "Laciesha");
+addImageCardToDOM("https://picsum.photos/id/112/400/300", "Wheat", "Sarah");
+addImageCardToDOM("https://picsum.photos/id/114/400/300", "Sky", "Adwaina");
