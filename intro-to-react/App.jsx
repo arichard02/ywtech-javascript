33ebbfa4d0a47f7f391abce34b643e5b618de43d
Adwaina <arichard02@aol.com>
2024-04-11 20:42:42 -0400

    deleted old code and added new code in App.jsx

--- intro-to-react/App.jsx
+++ intro-to-react/App.jsx
@@ -1,33 +1,33 @@
 import React from "react";
 import {Image, Carousel} from "antd";
+import "./App.css";
 
 export default function App() {
-		const carouselStyling = {
-				"width": "500px",
-				"backgroundColor": "#999",
-                "height": "300px"
-		};
-        const slideStyling={"height": "300px"}
     return (
         <>
             <header>
                 <h1>My First App</h1>
             </header>
-            <main>
-                <p>Hello React!</p>
-                <h2>Here are some images</h2>
-                <Image src="https://picsum.photos/400/400?id=1" width={200} />
-                <Image src="https://picsum.photos/400/400?id=2" width={200} />
-                <Image src="https://picsum.photos/400/400?id=3" width={200} />
-                
-                <h2>Here is a carousel</h2>
-                <div style={carouselStyling}>
-		                <Carousel>
-				                <div style={slideStyling}>Slide 1</div>
-				                <div style={slideStyling}>Slide 2</div>
-				                <div style={slideStyling}>Slide 3</div>
-		                </Carousel>
-	               </div>
+            <main> 
+                               
+                <div className="image-container">
+                    <h2>Here are some images</h2>
+                    <Image src="https://picsum.photos/400/400?id=1" width={200} />
+                    <Image src="https://picsum.photos/400/400?id=2" width={200} />
+                    <Image src="https://picsum.photos/400/400?id=3" width={200} />
+                    <Image src="https://picsum.photos/400/400?id=4" width={200} />
+                </div>
+
+                <div className="carousel-container">
+                    <h2>Here is a carousel</h2>
+                    <Carousel>
+                        <img src="https://picsum.photos/500/300?id=9" alt="Alt text" />
+                        <img src="https://picsum.photos/500/300?id=10" alt="Alt text" />
+                        <img src="https://picsum.photos/500/300?id=11" alt="Alt text" />
+                        <img src="https://picsum.photos/500/300?id=12" alt="Alt text" />
+                        <img src="https://picsum.photos/500/300?id=13" alt="Alt text" />
+                    </Carousel>
+                </div>
             </main>
         </>
     );
