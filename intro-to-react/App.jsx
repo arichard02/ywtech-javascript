057b4ae0edc736206bb0f198227753fa13658038
Adwaina <arichard02@aol.com>
2024-04-11 21:12:18 -0400

    added images to both containers

--- intro-to-react/App.jsx
+++ intro-to-react/App.jsx
@@ -16,6 +16,11 @@ export default function App() {
                     <Image src="https://picsum.photos/400/400?id=2" width={200} />
                     <Image src="https://picsum.photos/400/400?id=3" width={200} />
                     <Image src="https://picsum.photos/400/400?id=4" width={200} />
+                    <Image src="https://picsum.photos/400/400?id=5" width={200} />
+                    <Image src="https://picsum.photos/400/400?id=6" width={200} />
+                    <Image src="https://picsum.photos/400/400?id=7" width={200} />
+                    <Image src="https://picsum.photos/400/400?id=8" width={200} />
+                    <Image src="https://picsum.photos/400/400?id=9" width={200} />
                 </div>
 
                 <div className="carousel-container">
@@ -26,6 +31,10 @@ export default function App() {
                         <img src="https://picsum.photos/500/300?id=11" alt="Alt text" />
                         <img src="https://picsum.photos/500/300?id=12" alt="Alt text" />
                         <img src="https://picsum.photos/500/300?id=13" alt="Alt text" />
+                        <img src="https://picsum.photos/500/300?id=14" alt="Alt text" />
+                        <img src="https://picsum.photos/500/300?id=15" alt="Alt text" />
+                        <img src="https://picsum.photos/500/300?id=16" alt="Alt text" />
+                        <img src="https://picsum.photos/500/300?id=17" alt="Alt text" />
                     </Carousel>
                 </div>
             </main>
